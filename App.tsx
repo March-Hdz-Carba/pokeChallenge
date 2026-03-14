a8aebe03d7dedd658ba3354df4a840f7ee9221f5
March-Hdz-Carba <marcela.hdez.1801@gmail.com>
2026-03-14 11:56:28 -0600

    Add QueryClient

--- App.tsx
+++ App.tsx
@@ -1,24 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialDesignIcons } from '@react-native-vector-icons/material-design-icons';
 import HomeScreen from './views/HomeScreen';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function App() {
   const Tab = createBottomTabNavigator();
+  const queryClient = new QueryClient();
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={() => ({
-          tabBarIcon: () => <MaterialDesignIcons name='pokeball' color='#ff0000' size={20} />,
-          headerShown: false,
-        })}
-      >
-        <Tab.Screen name='Home' component={HomeScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={() => ({
+            tabBarIcon: () => <MaterialDesignIcons name='pokeball' color='#ff0000' size={30} />,
+            headerShown: false,
+          })}
+        >
+          <Tab.Screen name='Home' component={HomeScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
