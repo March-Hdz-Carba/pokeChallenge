7337a7d4b6ce39727f6c698ad0eaa494399a9b27
March-Hdz-Carba <marcela.hdez.1801@gmail.com>
2026-03-16 14:44:55 -0600

    Clean up

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,17 +2,14 @@ import { Image, StyleSheet } from 'react-native';
 
 export default function Title() {
   return (
-    <Image
-      source={require('../assets/title.png')}
-      style={styles.titleLogo}
-    />
+    <Image source={require('../assets/title.png')} style={styles.titleLogo} />
   );
 }
 
 const styles = StyleSheet.create({
   titleLogo: {
     height: '45%',
-    width: '70%',
     marginBottom: '5%',
+    width: '70%'
   }
 });
